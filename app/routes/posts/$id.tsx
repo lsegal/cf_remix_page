c3927380bf87672d26133ed8e4749b2323f63444
Loren Segal <lsegal@soen.ca>
2023-03-16 19:19:16 -0700

    Fixes

--- app/routes/posts/$id.tsx
+++ app/routes/posts/$id.tsx
@@ -3,28 +3,17 @@ import { json } from "@remix-run/cloudflare";
 import { Links, Meta, Outlet, Scripts, useLoaderData } from "@remix-run/react";
 
 export const loader = async ({ context, params }: LoaderArgs) => {
-  console.log(context);
-  const kv = context.KV as {
-    get: (key: string, args: any) => Promise<{ name: string }>;
-  };
-  return json(
-    await kv.get(`product-${params.id}`, {
-      type: "json",
-    })
-  );
+  const kv = context.KV as KVNamespace;
+  return await kv.get(`item-${params.id}`);
 };
 
 export default function Product() {
   const product = useLoaderData<typeof loader>();
 
-  if (!product) throw new Response(null, { status: 404 });
-
   return (
     <div>
       <p>Product</p>
-      {product.name}
-      <p>Products</p>
-      {/* ... */}
+      {product}
     </div>
   );
 }
